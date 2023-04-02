dd910bb4ea283693594ab6fc10d9ea3d9138f229
narendrapadala <narendrapadala@gmail.com>
2023-04-01 17:39:15 -0700

    Update run.sh
    updated port

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ sudo docker ps -f name=docker-kidzeebalagere-static -q | xargs --no-run-if-empty
 
 sudo docker container ls -a -fname=docker-kidzeebalagere-static -q | xargs -r docker container rm
 
-sudo docker run --expose=2025 -p 2025:80 -d --name docker-kidzeebalagere-static kidzeebalagere-static:version-1.1
+sudo docker run --expose=2028 -p 2028:80 -d --name docker-kidzeebalagere-static kidzeebalagere-static:version-1.1
